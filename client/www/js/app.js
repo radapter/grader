73986ad3db42bf7557d63392c3801901d22864ac
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-05-02 00:08:32 -0700

    Login/signup page done, add other pages structure

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -2,90 +2,175 @@
     "use strict";
 
     var gApoint = 90.0;
-
-    var computeGrade = function()
-    {
-        var currentPoints = Number( $('#points').val() );
-        var currentGrade = "NA";
-
-        if (currentPoints >= gApoint)
-        {
-            currentGrade = "A";
-        }
-        else
-        {
-            currentGrade = "F";
-        }
-        $('#finalgrade').text(currentGrade);
-    };
-
-    var saveSettings = function()
-    {
-        try {
-            var aPoint = parseFloat( $('#gradeCutOff').val() );
-
-            localStorage.setItem('gradeCutOff', aPoint);
-            gApoint = aPoint;
-            window.history.back();
-        } catch (ex)
-        {
-            alert('Points must be a decimal value');
-        }
-    };
-
-    var cancelSettings = function()
-    {
-        localStorage.clear();
-    };
-
-    var getlocJson = function () {
-
-        //$.ajax({
-        //    method: "POST",
-        //    url: "http://localhost:3000/login?email=jj@me.com&password=1234",
-        //    success: function(data){
-        //        console.log(data);
-        //    },
-        //    error: function (err) {
-        //        console.log("in error");
-        //        console.log(err);
-        //    }
-        //
-        //});
-
-        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=san jose', function (data) {
-            console.log(data);
-
-            $('#loc').text(data.results[0].formatted_address);
-        })
-    };
-
+    var currentUser ={};
 
     // Setup the event handlers
     $( document ).on( "ready", function()
     {
+        //sessionStorage.clear();
         $('#computeGrade').on('click', computeGrade);
         $('#saveSettings').on('click', saveSettings);
         $('#cancelSettings').on('click', cancelSettings);
 
         $('#testJson').on('click', getlocJson);
 
+        //login button handler
+        $('#loginBtn').on('click', login);
+        //signup button handler
+        $('#signupBtn').on('click', signup);
+
+
+        //TODO control logged in user info and show/hide different components in pages
+
         var gradeCutOffSetting = localStorage.getItem('gradeCutOff');
 
-        if (gradeCutOffSetting)
-        {
+        if (gradeCutOffSetting) {
             gApoint = parseFloat(gradeCutOffSetting);
         }
-
         $('#gradeCutOff').val(gApoint);
 
     });
 
     // Load plugin
     $( document ).on( "deviceready", function(){
-        StatusBar.overlaysWebView( false );
-        StatusBar.backgroundColorByName("gray");
+        //StatusBar.overlaysWebView( false );
+        //StatusBar.backgroundColorByName("gray");
     });
+
+    var computeGrade = function() {
+        var currentPoints = Number( $('#points').val() );
+        var currentGrade = "NA";
+
+        if (currentPoints >= gApoint) {
+            currentGrade = "A";
+        } else {
+            currentGrade = "F";
+        }
+        $('#finalgrade').text(currentGrade);
+    };
+
+    var saveSettings = function() {
+        try {
+            var aPoint = parseFloat( $('#gradeCutOff').val() );
+
+            localStorage.setItem('gradeCutOff', aPoint);
+            gApoint = aPoint;
+            window.history.back();
+        } catch (ex) {
+            alert('Points must be a decimal value');
+        }
+    };
+
+    var cancelSettings = function() {
+        localStorage.clear();
+    };
+
+    //test function
+    var getlocJson = function () {
+        $.ajax({
+            method: "POST",
+            dataType: "application/x-www-form-urlencoded",
+            url: "http://localhost:3000/users/login",
+            data:"email=ddd@ddd.com&password=whatever",
+            success: function(data){
+                console.log(data);
+            },
+            error: function(err){
+                console.log(err);
+            }
+        });
+        //$.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=san jose', function (data) {
+        //    console.log(data);
+        //
+        //    $('#loc').text(data.results[0].formatted_address);
+        //})
+    };
+
+    var login = function (e) {
+        e.preventDefault();
+
+        var formdata = $('#loginForm').serialize();
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/users/login",
+            data:formdata,
+            success: function(data){
+                console.log("login success");
+                console.log(data);
+
+                sessionStorage.setItem('currentUser', JSON.stringify(data.user));
+
+                currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+                console.log(currentUser.email);
+
+
+                preparePages(currentUser);
+
+                //login success, redirect to main page
+                $.mobile.changePage($('#userhome'), 'slide', true, true);
+
+            },
+            error: function(err){
+                console.log("login failed");
+                console.log(err.responseText);
+                //TODO, login error, show alert dialog
+            }
+        });
+    };
+
+    var signup = function (e) {
+        e.preventDefault();
+
+        var formdata = $('#signupForm').serialize();
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/users/signup",
+            data:formdata,
+            success: function(data){
+                console.log("signup success");
+
+                console.log(data);
+
+                sessionStorage.setItem('currentUser', JSON.stringify(data.user));
+
+                currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+                console.log(currentUser.email);
+
+                preparePages(currentUser.userType);
+
+                //login success, redirect to main page
+                $.mobile.changePage($('#userhome'), 'slide', true, true);
+
+            },
+            error: function(err){
+                console.log("signup failed");
+                console.log(err.responseText);
+                //TODO, signup error, show alert dialog
+            }
+        });
+    };
+
+    //prepare pages based on user info
+    var preparePages = function (currentUser) {
+
+        $("#userFname").html(currentUser.fname);
+
+        if(currentUser.userType === "teacher") {
+
+            //userhome page setting
+            //hide enrolBtn
+            $("#goEnrollCourseBtn").hide();
+            //show createBtn
+            $("#gocCreateCourseBtn").show();
+
+        } else {
+            //userhome page setting
+            $("#goEnrollCourseBtn").show();
+            $("#gocCreateCourseBtn").hide();
+        }
+    }
+
+
 }
 
 
