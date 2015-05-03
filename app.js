12a5079cf6fbddac3b3047b9fbe49ba89a9cd8a4
Tuo Lei <leituo56@gmail.com>
2015-05-02 17:35:11 -0700

    put client to node server

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/www')));
 app.use(cors());
 app.use(session({
   secret: 'CMPE235 Grader',
