9678cc03d1162a193b92b67e2c4b2c44817fa8d6
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-05-03 00:22:00 -0700

    avatars, homepage switch, userhome courseList template, createCourse - add avatars icons - homepage switch between login/logout status, logout function - userhome corsets, click delegate event to pass course id - createCourse page and funcion - fix bug of show panel - !! trigger('create') for dynmically adding element

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -12,16 +12,34 @@
         $('#computeGrade').on('click', computeGrade);
         $('#saveSettings').on('click', saveSettings);
         $('#cancelSettings').on('click', cancelSettings);
+        $('#createCourseBtn').on('click', createCourse);
+
+        //delegate click event on course panel, aim to pass courseid
+        $('#userCourseList').delegate('.coursePanel', 'click', function () {
+            console.log($(this).attr('id'));
+
+        });
 
         $('#testJson').on('click', getlocJson);
 
         //login button handler
         $('#loginBtn').on('click', login);
+        //logout button handler
+        $('#logoutBtn').on('click', logout);
         //signup button handler
         $('#signupBtn').on('click', signup);
 
 
         //TODO control logged in user info and show/hide different components in pages
+        currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+        if(currentUser) {
+            console.log(currentUser.email);
+            prepareUserPages(currentUser);
+        } else {
+            $('#beforeLoginHome').show();
+            $('#afterLoginHome').hide();
+        }
+
 
         var gradeCutOffSetting = localStorage.getItem('gradeCutOff');
 
@@ -31,6 +49,7 @@
         $('#gradeCutOff').val(gApoint);
 
     });
+
 
     // Load plugin
     $( document ).on( "deviceready", function(){
@@ -106,7 +125,7 @@
                 console.log(currentUser.email);
 
 
-                preparePages(currentUser);
+                prepareUserPages(currentUser);
 
                 //login success, redirect to main page
                 $.mobile.changePage($('#userhome'), 'slide', true, true);
@@ -119,6 +138,28 @@
             }
         });
     };
+
+    var logout = function () {
+        $.ajax({
+            method: "POST",
+            url: urlroot + "/users/logout",
+            success: function(data){
+                console.log("logout success");
+                console.log(data);
+
+                //store current user to sessionStorage
+                sessionStorage.clear();
+                $('#beforeLoginHome').show();
+                $('#afterLoginHome').hide();
+            },
+            error: function(err){
+                console.log("login failed");
+                console.log(err.responseText);
+                //TODO, login error, show alert dialog
+            }
+        });
+    };
+
 
     var signup = function (e) {
         e.preventDefault();
@@ -143,7 +184,7 @@
                 currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
                 console.log(currentUser.email);
 
-                preparePages(currentUser.userType);
+                prepareUserPages(currentUser);
 
                 //login success, redirect to main page
                 $.mobile.changePage($('#userhome'), 'slide', true, true);
@@ -157,15 +198,93 @@
         });
     };
 
+    var createCourse = function (e) {
+        e.preventDefault();
+
+        //build course data to json
+        var formdata = $("#createCourseForm").serializeArray();
+        console.log(formdata);
+
+        var courseObj = {};
+        courseObj.name = $("input[name=name]").val();
+        courseObj.description = $("textarea[name=description]").val();
+
+        var meta ={};
+        var Homeworks ={};
+        Homeworks.max = $("input[name=HomeworksMax]").val();
+        Homeworks.factor = $("input[name=HomeworksFactor]").val();
+        var Labs ={};
+        Labs.max = $("input[name=LabsMax]").val();
+        Labs.factor = $("input[name=LabsFactor]").val();
+        var Project ={};
+        Project.max = $("input[name=ProjectMax]").val();
+        Project.factor = $("input[name=ProjectFactor]").val();
+        var Presentation ={};
+        Presentation.max = $("input[name=PresentationMax]").val();
+        Presentation.factor = $("input[name=PresentationFactor]").val();
+        var Midterm ={};
+        Midterm.max = $("input[name=MidtermMax]").val();
+        Midterm.factor = $("input[name=MidtermFactor]").val();
+        var Final ={};
+        Final.max = $("input[name=FinalMax]").val();
+        Final.factor = $("input[name=FinalFactor]").val();
+
+        meta.Homeworks = Homeworks;
+        meta.Labs = Labs;
+        meta.Project = Project;
+        meta.Presentation = Presentation;
+        meta.Midterm = Midterm;
+        meta.Final = Final;
+
+        courseObj.meta = meta;
+
+        var policy ={};
+        policy.A = $("input[name=PolicyA]").val();
+        policy.B = $("input[name=PolicyB]").val();
+        policy.C = $("input[name=PolicyC]").val();
+        policy.D = $("input[name=PolicyD]").val();
+
+        courseObj.policy = policy;
+
+        console.log(courseObj);
+
+        $.ajax({
+            url:urlroot+"/courses",
+            type: "POST",
+            data: JSON.stringify(courseObj),
+            contentType: "application/json",
+            success: function(data){
+                console.log(data.course);
+
+                //TODO goto course detail page
+            },
+            error: function(err){
+                console.log(err);
+            }
+        });
+
+    };
+
+
+    function goCourseDetail(courseId) {
+        console.log(courseId);
+    }
+
     //prepare pages based on user info
-    var preparePages = function (currentUser) {
+    var prepareUserPages = function (currentUser) {
+
+        $('#beforeLoginHome').hide();
+        $('#afterLoginHome').show();
 
         $("#userFname").html(currentUser.fname);
 
-
         //userCourse list template
         var userCoursesTmp = _.template($("script#userCourseListTmp").html());
-        _.templateSettings.variable = "rc";
+
+        //load user profile
+        var userProfileTmp = _.template($("script#userProfileTmp").html());
+        currentUser.avatarUrl = "./img/avatars/"+currentUser.avatarId +".png";
+        $("#userProfilePanel").html(userProfileTmp(currentUser));
 
 
         if(currentUser.userType === "teacher") {
@@ -183,6 +302,9 @@
                 console.log(userCourseList);
                 //data.courses
                 $("#userCourseList").html(userCoursesTmp(userCourseList));
+
+                //!!apply styles after dynamically adding element
+                $("#userCourseList").trigger('create');
             });
 
         } else {
@@ -196,6 +318,9 @@
                 console.log(userCourseList);
                 //data.enrolls
                 $("#userCourseList").html(userCoursesTmp(userCourseList));
+
+                //!!apply styles after dynamically adding element
+                $("#userCourseList").trigger('create');
 
             });
         }
